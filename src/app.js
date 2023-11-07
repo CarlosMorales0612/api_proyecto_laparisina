7c981ecf20eb7724de0a34b535fce8aa473eb14a
Aagudelo18 <alejandraagudelocartagena@gmail.com>
2023-11-07 10:18:52 -0500

    ultima edicion

--- src/app.js
+++ src/app.js
@@ -1,19 +1,15 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const authRoutes = require('./routes/authRoutes');
+
 const categoriaRoutes = require('./routes/categoriaRoutes');
 const productoRoutes = require('./routes/productoRoutes')
 const ordenDeProduccionRoutes = require('./routes/ordenDeProduccionRoutes')
 const pedidoRoutes = require('./routes/pedidoRoutes');
 const usuarioRoutes = require('./routes/usuarioRoutes');
-const ClientesRoutes = require('./routes/ClientesRoutes');
+const ClientesRoutes= require('./routes/ClientesRoutes');
 const RolesRoutes = require('./routes/RolesRoutes');
-<<<<<<< HEAD
-const empleadoRoutes = require('./routes/empleadoRoutes')
-=======
-const ventasRoutes = require('./routes/ventasRoutes');
->>>>>>> 607ba1972808d06cfbb1732b386fe08780911714
+const EmpleadoRoutes = require('./routes/empleadoRoutes');
 const cors = require('cors')
 
 app.use(cors())
@@ -25,10 +21,6 @@ app.use(express.static('uploads'))
 
 // Conectar las rutas
 
-<<<<<<< HEAD
-app.use('/api',empleadoRoutes,ClientesRoutes,RolesRoutes, categoriaRoutes,productoRoutes,ordenDeProduccionRoutes,pedidoRoutes,usuarioRoutes);
-=======
-app.use('/api', authRoutes, ClientesRoutes, RolesRoutes, categoriaRoutes, productoRoutes, ordenDeProduccionRoutes, pedidoRoutes, usuarioRoutes, ventasRoutes);
->>>>>>> 607ba1972808d06cfbb1732b386fe08780911714
+app.use('/api',EmpleadoRoutes,ClientesRoutes,RolesRoutes, categoriaRoutes,productoRoutes,ordenDeProduccionRoutes,pedidoRoutes,usuarioRoutes);
 
 module.exports = app;
