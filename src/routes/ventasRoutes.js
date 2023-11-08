a2bb075613d961ae5f99058b5f7497b2feffd5b8
isaaacmz79 <medebes20000@gmail.com>
2023-11-08 08:03:10 -0500

    Modulo organizado de usuario con método  referenciando, trayendo el populate

--- src/routes/ventasRoutes.js
+++ src/routes/ventasRoutes.js
@@ -5,4 +5,7 @@ const VentasController = require('../controllers/VentasController');
 // Ruta para obtener todas las ventas
 router.get('/ventas', VentasController.getVentas);
 
+//Ruta para obtener venta por id
+router.get('/ventas/:id', VentasController.getVentaById);
+
 module.exports = router;
