3f2b6b11ffcd6679428745a8edd3b8205a1a6651
CarlosMorales0612 <charlied0612@gmail.com>
2023-11-21 08:39:01 -0500

    actualizacion api pedidos

--- src/controllers/PedidoController.js
+++ src/controllers/PedidoController.js
@@ -33,24 +33,20 @@ async function createPedido(req, res) {
   try {
     const pedidoData = req.body;
 
+    pedidoData.fecha_pedido_tomado = new Date().toISOString().split('T')[0]
+
     // Validar que el campo 'nombre_cliente' solo contenga letras
     if (!/^[A-Za-z\s]+$/.test(pedidoData.nombre_cliente)) {
       return res.status(400).json({ error: 'El nombre del cliente solo debe contener letras.' });
     }
      // Validar que el campo 'nombre_cliente' solo contenga letras
-     if (!/^[A-Za-z\s]+$/.test(pedidoData.quien_recibe)) {
-      return res.status(400).json({ error: 'El nombre del Quien recibe solo debe contener letras.' });
-    }
+    //  if (!/^[A-Za-z\s]+$/.test(pedidoData.quien_recibe)) {
+    //   return res.status(400).json({ error: 'El nombre del Quien recibe solo debe contener letras.' });
+    // }
       // Validar que el campo 'telefono_cliente' solo contenga números
     if (!/^\d+$/.test(pedidoData.telefono_cliente)) {
       return res.status(400).json({ error: 'El teléfono solo debe contener números.' });
     }
-    
-    // // Validar que el campo 'fecha_entrega_pedido' tiene el formato 'día/mes/año'
-    // if (!/^\d{1,2}\/\d{1,2}\/\d{4}$/.test(pedidoData.fecha_entrega_pedido)) {
-    //   return res.status(400).json({ error: 'El formato de la fecha de entrega no es válido. Debe ser en formato día/mes/año.' });
-    // }
-
     // Validar que el pedido tenga al menos un producto en el detalle_pedido
     if (!pedidoData.detalle_pedido || pedidoData.detalle_pedido.length === 0) {
       return res.status(400).json({ error: 'Debes agregar al menos un producto al pedido.' });
@@ -64,32 +60,15 @@ async function createPedido(req, res) {
         }
       }
     }
-  //     // Validar que el estado del pedido solo contenga letras
-  //  if (!/^[a-zA-Z]+$/.test(pedidoData.estado_pedido)) {
-  //   return res.status(400).json({ error: 'El estado del pedido solo debe contener letras.' });
-  // }
-
-    // Validar que el nuevo estado sea válido (debe estar en la lista de estados permitidos)
-    // const estadosPermitidos = ['tomado', 'preparacion', 'terminado', 'asignado', 'enviado', 'entregado', 'anulado'];
-    // if (!estadosPermitidos.includes(pedidoData.estado_pedido)) {
-    //   return res.status(400).json({
-    //     error: 'Estado no permitido. Los estados permitidos son: ' + estadosPermitidos.join(', ')
-    //   });
-    // }
-   
-    
     const nuevoPedido = new Pedido(pedidoData);
 
     await nuevoPedido.save();
 
     res.status(201).json({ message: 'Pedido creado exitosamente', pedido: nuevoPedido });
   } catch (error) {
-    if (error.code === 11000 && error.keyPattern.codigo_cliente) {
-      res.status(400).json({ error: `El cliente con código ${req.body.codigo_cliente} ya tiene un pedido.` });
-    } else {
-      // Otro tipo de error, envía una respuesta de error genérica
-      res.status(500).json({ error: 'Error al crear el pedido.' });
-    }
+   
+      res.status(500).json({ error: 'Error al crear el pedido.', error });
+  
   }
 }
  
@@ -151,6 +130,8 @@ async function deletePedido(req, res) {
     res.status(500).json({ error: 'Error al eliminar el pedido.' });
   }
 }
+
+
 
 
 
--- src/models/Pedido.js
+++ src/models/Pedido.js
@@ -4,7 +4,11 @@ const pedidoSchema = new mongoose.Schema({
 
   documento_cliente: {
     type: String,
-    required: [true, 'El campo código_cliente es requerido.'],
+    required: [true, 'El campo documento es requerido.'],
+  },
+  tipo_cliente: {
+    type: String,
+    required: [true, 'El campo tipo cliente es requerido.'],
   },
   
    nombre_contacto: {
@@ -18,7 +22,6 @@ const pedidoSchema = new mongoose.Schema({
   },
   quien_recibe: {
     type: String,
-    required: [true, 'El campo quien recibe es requerido.']
   },
   direccion_entrega: {
     type: String,
@@ -28,19 +31,22 @@ const pedidoSchema = new mongoose.Schema({
     type: String,
     required: [true, 'El campo ciudad es requerido.']
   },
-  ciudad_barrio: {
+  barrio_cliente: {
     type: String,
-    required: [true, 'El campo ciudad es requerido.']
+    required: [true, 'El campo barrio es requerido.']
   },
   fecha_entrega_pedido: {
-    type: String,
+    type: Date,
     required: [true, 'El campo fecha entrega pedido es requerido.']
+  },
+  fecha_pedido_tomado: {
+    type: Date,
   },
   estado_pedido: {
     type: String,
     required: true,
     enum: ['Pendiente', 'Tomado', 'Preparacion', 'Terminado', 'Asignado', 'Enviado', 'Entregado', 'Anulado'],
-    default: ['Pendiente']
+    default: 'Pendiente'
   },
   precio_total_venta: {
     type: Number,
@@ -55,8 +61,19 @@ const pedidoSchema = new mongoose.Schema({
     enum: ['Efectivo', 'Transferencia'],
     required: [true, 'El campo metodo pago es requerido.']
   },
+  
   valor_domicilio: {
     type: Number
+  },
+  nit_empresa_cliente: {
+    type: String,
+  },
+  nombre_juridico: {
+    type: String,
+  },
+  aumento_empresa: {
+    type: Number,
+    
   },
 
   detalle_pedido: [
@@ -64,14 +81,6 @@ const pedidoSchema = new mongoose.Schema({
       nombre_producto: {
         type: String,
         required: [true, 'El campo nombre producto es requerido.']
-      },
-      nombre_categoria_producto: {
-        type: String,
-        required: [true, 'El campo nombre categoria producto es requerido.']
-      },
-      fecha_pedido_tomado: {
-        type: String,
-        required: [true, 'El campo fecha pedido tomado es requerido.']
       },
       cantidad_producto: {
         type: Number,
@@ -85,11 +94,15 @@ const pedidoSchema = new mongoose.Schema({
         type: Number,
         required: [true, 'El campo precio ico es requerido.']
       },
+      precio_por_mayor_ico:{
+        type: Number,
+        required: [true, 'El campo precio ico es requerido.']
+      },
       precio_total_producto: {
         type: Number,
         required: [true, 'El campo precio total_producto es requerido.']
-      }
-
+      },
+     
     }
   ]
 });
