004d55c2eb06cad31edc19cd6c1ab82677a4df67
isaaacmz79 <medebes20000@gmail.com>
2023-12-10 17:46:41 -0500

    Ventas con excel

--- src/controllers/VentasController.js
+++ src/controllers/VentasController.js
@@ -34,6 +34,112 @@ async function getVentaById(req, res) {
   }
 }
 
+const ventasGetExcel = async (req, res = response) => {
+  try {
+    const query = { estado_pedido: 'Entregado' };
+    const ventas = await Pedido.find(query);
+
+    if (!ventas || ventas.length === 0) {
+      return res.status(404).json({ message: 'No se encontraron ventas' });
+    }
+
+    const workbook = new exceljs.Workbook();
+    const worksheet = workbook.addWorksheet('Ventas');
+
+    const columnas = [
+      'Documento Cliente',
+      'Tipo Cliente',
+      'Nombre Contacto',
+      'Teléfono Cliente',
+      'Quién Recibe',
+      'Dirección Entrega',
+      'Ciudad Cliente',
+      'Barrio Cliente',
+      'Fecha Entrega Pedido',
+      'Estado Pedido',
+      'Precio Total Venta',
+      'Subtotal Venta',
+      'Método de Pago',
+      'Valor Domicilio',
+      'Correo Domiciliario',
+      'NIT Empresa Cliente',
+      'Nombre Jurídico',
+      'Aumento Empresa',
+      'Nombre Domiciliario',
+      // Columnas para los detalles del pedido
+      'Nombre Producto 1',
+      'Cantidad Producto 1',
+      'Estado Producto 1',
+      'Precio ICO Producto 1',
+      'Precio Por Mayor ICO Producto 1',
+      'Precio Total Producto 1',
+      'Nombre Producto 2',
+      'Cantidad Producto 2',
+      'Estado Producto 2',
+      'Precio ICO Producto 2',
+      'Precio Por Mayor ICO Producto 2',
+      'Precio Total Producto 2',
+      // Asegúrate de ajustar y agregar más columnas si hay más detalles esperados
+    ];
+    worksheet.addRow(columnas);
+
+    ventas.forEach(venta => {
+      const datosVenta = [
+        venta.documento_cliente,
+        venta.tipo_cliente,
+        venta.nombre_contacto,
+        venta.telefono_cliente,
+        venta.quien_recibe,
+        venta.direccion_entrega,
+        venta.ciudad_cliente,
+        venta.barrio_cliente,
+        venta.fecha_entrega_pedido,
+        venta.estado_pedido,
+        venta.precio_total_venta,
+        venta.subtotal_venta,
+        venta.metodo_pago,
+        venta.valor_domicilio,
+        venta.correo_domiciliario,
+        venta.nit_empresa_cliente,
+        venta.nombre_juridico,
+        venta.aumento_empresa,
+        venta.nombre_domiciliario,
+        // Datos de la venta
+
+        // Detalles del primer producto
+        (venta.detalle_pedido[0] || {}).nombre_producto || '',
+        (venta.detalle_pedido[0] || {}).cantidad_producto || '',
+        (venta.detalle_pedido[0] || {}).estado_producto || '',
+        (venta.detalle_pedido[0] || {}).precio_ico || '',
+        (venta.detalle_pedido[0] || {}).precio_por_mayor_ico || '',
+        (venta.detalle_pedido[0] || {}).precio_total_producto || '',
+
+        // Detalles del segundo producto
+        (venta.detalle_pedido[1] || {}).nombre_producto || '',
+        (venta.detalle_pedido[1] || {}).cantidad_producto || '',
+        (venta.detalle_pedido[1] || {}).estado_producto || '',
+        (venta.detalle_pedido[1] || {}).precio_ico || '',
+        (venta.detalle_pedido[1] || {}).precio_por_mayor_ico || '',
+        (venta.detalle_pedido[1] || {}).precio_total_producto || '',
+        // Asegúrate de ajustar para manejar más detalles si son posibles
+      ];
+
+      worksheet.addRow(datosVenta);
+    });
+
+    res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+    res.setHeader('Content-Disposition', 'attachment; filename=ventas.xlsx');
+
+    await workbook.xlsx.write(res);
+    res.end();
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error en el servidor' });
+  }
+};
+
+
+
 // //Funcion para el excel-------------------------------------------------------------------------------------------------------------------------
 // const ventaGetexcel = async (req, res = response) => {
 //   try {
@@ -89,5 +195,6 @@ async function getVentaById(req, res) {
 
 module.exports = {
   getVentas,
-  getVentaById
+  getVentaById,
+  ventasGetExcel
 }
