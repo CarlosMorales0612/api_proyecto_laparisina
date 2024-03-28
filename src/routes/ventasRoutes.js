ab875f5f77790ea75a660455137ccbd206b43e50
isaaacmz79 <medebes20000@gmail.com>
2024-03-27 21:58:18 -0500

    Ventas terminada

--- src/routes/ventasRoutes.js
+++ src/routes/ventasRoutes.js
@@ -4,11 +4,12 @@ const VentasController = require('../controllers/VentasController');
 const { validarJWT, permiso_ventas } = require('../middlewares/index');
 
 // Ruta para obtener todas las ventas
-router.get('/ventas', [validarJWT, permiso_ventas], VentasController.getVentas);
+router.get('/ventas',  VentasController.getVentas);
 
 //Ruta para obtener venta por id
 router.get('/ventas/:id', [validarJWT, permiso_ventas], VentasController.getVentaById);
 
+//Ruta para generar un excel sobre las ventas
 router.get('/ventas_excel', [], VentasController.ventasGetExcel);
 
 module.exports = router;
